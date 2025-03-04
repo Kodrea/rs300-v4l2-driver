24fdd6f5213da58e64cbca948f8743e9e6b170ba
Kodrea <81979138+Kodrea@users.noreply.github.com>
2025-03-03 19:22:52 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/bash
 
 DRV_VERSION=0.0.1
-DRV_NAME=rs300
+DRV_IMX=rs300
 
-echo "Uninstalling any previous ${DRV_NAME} module"
-sudo dkms remove -m ${DRV_NAME} -v ${DRV_VERSION} --all
+echo "Uninstalling any previous ${DRV_IMX} module"
+sudo dkms remove -m ${DRV_IMX} -v ${DRV_VERSION} --all
 
-sudo mkdir -p /usr/src/${DRV_NAME}-${DRV_VERSION}
+sudo mkdir -p /usr/src/${DRV_IMX}-${DRV_VERSION}
 
-sudo cp -r $(pwd)/* /usr/src/${DRV_NAME}-${DRV_VERSION}
+sudo cp -r $(pwd)/* /usr/src/${DRV_IMX}-${DRV_VERSION}
 
-sudo dkms add -m ${DRV_NAME} -v ${DRV_VERSION}
-sudo dkms build -m ${DRV_NAME} -v ${DRV_VERSION}
-sudo dkms install -m ${DRV_NAME} -v ${DRV_VERSION}
+sudo dkms add -m ${DRV_IMX} -v ${DRV_VERSION}
+sudo dkms build -m ${DRV_IMX} -v ${DRV_VERSION}
+sudo dkms install -m ${DRV_IMX} -v ${DRV_VERSION}
